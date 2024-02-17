ad670d0ceed41ba07c3c211985da579fd6899fc6
renofapar <renofapar@gmail.com>
2024-02-17 19:15:18 +0300

    change file

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Post from './post'
-import './assets/styles/main.scss';
-const post = new Post('webpack post title')
-const getElement = () => console.log('getElement')
-document.addEventListener('click', getElement)
-console.log(post.toString())
